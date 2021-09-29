e69a8bd62ec17a979e01ac2b4b33ed5bb2ef2d9c
Alexandr Boyko <BoykoA.A@yandex.ru>
2021-09-29 20:01:50 +0300

    Update abstract_executor.py

--- strategy/tools/abstract_tools/abstract_executor.py
+++ strategy/tools/abstract_tools/abstract_executor.py
@@ -5,5 +5,13 @@ class AbstractExecutor(ABC):
     """Executor interface"""
 
     @abstractmethod
-    def execute(self, actions: list) -> bool:
+    def execute(self, actions: dict) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def make_market_order(self, ticker: str, side: str, quantity: float, **kwargs) -> dict:
+        raise NotImplementedError
+
+    @abstractmethod
+    def make_limit_order(self, ticker: str, side: str, price: float, quantity: float, reduce_only: bool, **kwargs) -> dict:
         raise NotImplementedError
