eb7ea2efedd7d7a3b217985814a996878aba2156
andreymaltsev <a.maltsev@hashcib.com>
2021-10-21 00:08:46 +0300

    fix delta

--- test_executor.py
+++ test_executor.py
@@ -1,7 +1,7 @@
 from strategy import FundingExecutor
 
-api_key = 'IdPorsZNdskqCUNbO5aN0w6TY67Kfl0syZjHDV3ZP9tOMuM6k3KzovNizMKmBpix'
-secret_key = '7qE1lC0fVpNF7i9Lb08odC1HaV6m2LILmzy2SSEnAXTwqOVaJhqA8cVz1tzPzP0A'
+api_key = ''
+secret_key = ''
 
 ticker_swap = 'ETHUSD_PERP'
 ticker_futures = 'ETHUSD_211231'
