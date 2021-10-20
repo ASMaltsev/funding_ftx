4958d57680e8ea4a9033c1822c73b319e865d4c5
Alexandr Boyko <BoykoA.A@yandex.ru>
2021-10-20 12:25:39 +0300

    up

--- test_executor.py
+++ test_executor.py
@@ -5,7 +5,7 @@ api_key = 'IdPorsZNdskqCUNbO5aN0w6TY67Kfl0syZjHDV3ZP9tOMuM6k3KzovNizMKmBpix'
 secret_key = '7qE1lC0fVpNF7i9Lb08odC1HaV6m2LILmzy2SSEnAXTwqOVaJhqA8cVz1tzPzP0A'
 
 ticker_swap = 'ETHUSD_PERP'
-ticker_futures = 'ETHUSD_211231'
+ticker_futures = 'ETHUSD_220325'
 
 swap_side = 'sell'
 futures_side = 'buy'
