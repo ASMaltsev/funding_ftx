badf9774aaf5f6fd3dc25e8ce34c931b2e773b72
Alexandr Boyko <BoykoA.A@yandex.ru>
2021-10-20 12:49:34 +0300

    up

--- test_executor.py
+++ test_executor.py
@@ -1,8 +1,8 @@
 from strategy import FundingExecutor
 
-#ShSh1
-api_key = 'cgi94unJMvafiMFPwcvy115hDxSIqVjecPtpUTFEno92ldJlG57x2vBVPzsWzbG9'
-secret_key = 'C6PiycF8lSHUuULY4BnvbpPPZinxs9vaFmlHGLazStYbW5qSTM96OsahQNR6dnR0'
+#YM
+api_key  = 'QRjSmxIXbjCujcDH4FFn9YobT9B0e52diGa68JbuAGCHUCoOqaq3Ppig4KhhX3i2'
+secret_key = 't4EFPmyOdhqxdMz27QUcikK4srXRrvmPgL4rlXWvFtrMR6bEjCAnXQIrhu8CFY79'
 
 ticker_swap = 'BTCUSD_PERP'
 ticker_futures = 'BTCUSD_211231'
@@ -10,7 +10,7 @@ ticker_futures = 'BTCUSD_211231'
 swap_side = 'sell'
 futures_side = 'buy'
 reduce_only = False
-total_amount = 8983
+total_amount = 4677
 
 FundingExecutor(api_key=api_key, secret_key=secret_key, section='COIN-M')._execute(
     market_ticker=ticker_futures,
