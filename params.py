caec58284491d4fd6b51138a092ba795c5930b2b
Alexandr Boyko <BoykoA.A@yandex.ru>
2021-10-25 18:38:03 +0300

    up

--- params.py
+++ params.py
@@ -1,5 +1,10 @@
-api_key = ''
-secret_key = ''
+
+
+
+
+# # TEST API
+api_key = 'IdPorsZNdskqCUNbO5aN0w6TY67Kfl0syZjHDV3ZP9tOMuM6k3KzovNizMKmBpix'
+secret_key = '7qE1lC0fVpNF7i9Lb08odC1HaV6m2LILmzy2SSEnAXTwqOVaJhqA8cVz1tzPzP0A'
 
 
 client_name = 'test'
@@ -10,5 +15,5 @@ ticker_futures = 'ETHUSD_220325'
 swap_side = 'buy'
 futures_side = 'sell'
 reduce_only = False
-total_amount = 4
+total_amount = 20
 section = 'COIN-M'
