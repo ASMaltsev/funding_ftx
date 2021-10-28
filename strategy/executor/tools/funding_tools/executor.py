a1d1967b4538a96c0c11a24aa991a257782bde7c
andreymaltsev <a.maltsev@hashcib.com>
2021-10-28 12:23:23 +0300

    update logs

--- strategy/executor/tools/funding_tools/executor.py
+++ strategy/executor/tools/funding_tools/executor.py
@@ -17,8 +17,8 @@ class FundingExecutor(AbstractExecutor):
         self.start_amount_market = 0
 
     def _work_before_new_limit_order(self, limit_ticker, market_ticker):
-        logger.debug(msg=colored('Rate limits:', 'blue'),
-                     extra=dict(current_rpc=self.data_provider.current_rpc, max_rpc=self.data_provider.max_rpc))
+        logger.info(msg=colored('Rate limits:', 'blue'),
+                    extra=dict(current_rpc=self.data_provider.current_rpc, max_rpc=self.data_provider.max_rpc))
         self._control_rpc()
         self._log_current_positions(limit_ticker, market_ticker)
 
