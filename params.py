0ad648a199cade14f08daeb56e7f0f34fa8c2062
Alexandr Boyko <BoykoA.A@yandex.ru>
2021-10-28 12:56:59 +0300

    up

--- params.py
+++ params.py
@@ -5,19 +5,20 @@
 
 
 
-#YM
-api_key  = 'QRjSmxIXbjCujcDH4FFn9YobT9B0e52diGa68JbuAGCHUCoOqaq3Ppig4KhhX3i2'
-secret_key = 't4EFPmyOdhqxdMz27QUcikK4srXRrvmPgL4rlXWvFtrMR6bEjCAnXQIrhu8CFY79'
+
+# # TEST API
+api_key = 'IdPorsZNdskqCUNbO5aN0w6TY67Kfl0syZjHDV3ZP9tOMuM6k3KzovNizMKmBpix'
+secret_key = '7qE1lC0fVpNF7i9Lb08odC1HaV6m2LILmzy2SSEnAXTwqOVaJhqA8cVz1tzPzP0A'
 
 
 
 client_name = 'YM'
 
-ticker_swap = 'BTCUSD_PERP'
-ticker_futures = 'BTCUSD_211231'
+ticker_swap = 'ETHUSDT'
+ticker_futures = 'ETHUSDT_211231'
 
 swap_side = 'sell'
 futures_side = 'buy'
 reduce_only = False
-total_amount = 2000
-section = 'COIN-M'
+total_amount = 0.2
+section = 'USDT-M'
