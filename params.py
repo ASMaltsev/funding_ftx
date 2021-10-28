b0d8402d98714e83675acc5835a01ed1bf144ca2
Alexandr Boyko <BoykoA.A@yandex.ru>
2021-10-28 12:43:22 +0300

    up

--- params.py
+++ params.py
@@ -13,8 +13,8 @@ secret_key = 'DF37JQmlVNZu0DnOeU8Yie0AAu7MZMprsleAeiRz5TEicv6wsADDhBUnHr9qc1Uv'
 
 client_name = 'shsh2'
 
-ticker_swap = 'ETHUSD_PERP'
-ticker_futures = 'ETHUSD_211231'
+ticker_swap = 'BTCUSD_PERP'
+ticker_futures = 'BTCUSD_211231'
 
 swap_side = 'sell'
 futures_side = 'buy'
