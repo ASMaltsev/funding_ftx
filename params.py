ad8a477926cfc85a4384846302be99758680a709
Alexandr Boyko <BoykoA.A@yandex.ru>
2021-10-28 13:52:45 +0300

    up

--- params.py
+++ params.py
@@ -5,13 +5,13 @@
 
 
 
-#ShSh1
-api_key = 'cgi94unJMvafiMFPwcvy115hDxSIqVjecPtpUTFEno92ldJlG57x2vBVPzsWzbG9'
-secret_key = 'C6PiycF8lSHUuULY4BnvbpPPZinxs9vaFmlHGLazStYbW5qSTM96OsahQNR6dnR0'
+#ShSh2
+api_key = 'OkL6vNlhn2g6xRT5rGCFZcigdlifeeDItakvzk5sfbgszo5WkQMOQPwT7hUKTMkH'
+secret_key = 'DF37JQmlVNZu0DnOeU8Yie0AAu7MZMprsleAeiRz5TEicv6wsADDhBUnHr9qc1Uv'
 
 
 
-client_name = 'shsh1'
+client_name = 'shsh2'
 
 ticker_swap = 'BTCUSD_PERP'
 ticker_futures = 'BTCUSD_211231'
@@ -19,5 +19,5 @@ ticker_futures = 'BTCUSD_211231'
 swap_side = 'sell'
 futures_side = 'buy'
 reduce_only = False
-total_amount = 5000
+total_amount = 5500
 section = 'COIN-M'
