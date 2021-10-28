c5dd482b3ea16509bf3e3b470a4d956b471e2a50
andreymaltsev <a.maltsev@hashcib.com>
2021-10-28 17:05:21 +0300

    update hyperparms

--- strategy/hyperparameters.py
+++ strategy/hyperparameters.py
@@ -1,30 +1,42 @@
 hyperparams = {
     'USDT-M':
         {
-            'leverage_perp': 8.0,
-            'leverage_quart': 8.0,
             'share':
                 {
-                    'BTC': 0.4,
-                    'ETH': 0.6,
+                    'BTC':
+                        {
+                            'leverage_perp': 8.0,
+                            'leverage_quart': 8.0,
+                        },
+                    'ETH':
+                        {
+                            'leverage_perp': 8.0,
+                            'leverage_quart': 8.0,
+                        }
                 }
         },
+
     'COIN-M':
         {
-            'leverage_perp': 8.0,
-            'leverage_quart_current': 8.0,
-            'leverage_quart_next': 8.0,
             'share':
                 {
                     'ETH':
                         {
                             'quart_current': 0.5,
                             'quart_next': 0.5,
+
+                            'leverage_perp': 8.0,
+                            'leverage_quart_current': 8.0,
+                            'leverage_quart_next': 8.0,
                         },
                     'BTC':
                         {
                             'quart_current': 0.5,
                             'quart_next': 0.5,
+
+                            'leverage_perp': 8.0,
+                            'leverage_quart_current': 8.0,
+                            'leverage_quart_next': 8.0,
                         }
                 }
         }
