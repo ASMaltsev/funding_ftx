1191b349fa678b9e61f2dc2153d2741228700308
Alexandr Boyko <BoykoA.A@yandex.ru>
2021-10-28 12:17:36 +0300

    up

--- params.py
+++ params.py
@@ -10,7 +10,7 @@ api_key = 'cgi94unJMvafiMFPwcvy115hDxSIqVjecPtpUTFEno92ldJlG57x2vBVPzsWzbG9'
 secret_key = 'C6PiycF8lSHUuULY4BnvbpPPZinxs9vaFmlHGLazStYbW5qSTM96OsahQNR6dnR0'
 
 
-client_name = 'test'
+client_name = 'shsh1'
 
 ticker_swap = 'BTCUSD_PERP'
 ticker_futures = 'BTCUSD_211231'
