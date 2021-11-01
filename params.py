2c1d21df1a7f5c158e865fee94f694605bafe6f2
Alexandr Boyko <BoykoA.A@yandex.ru>
2021-11-01 12:09:41 +0300

    up

--- params.py
+++ params.py
@@ -5,18 +5,18 @@
 
 
 
-#ML
-api_key = '6dASmxxyZ47TxvxVy6LtRv0HeZPGqn6ybfJqyQoveL2bYg8PPeMYzRs8lG3SGuVZ'
-secret_key = 'RuD9h5JGzPkpo6M182O3BMTo5em8eMZN62JbmDqHDrVSsQw6T865W6B2NHnNMzCj'
+#MP
+api_key = '68HCpwMnmG9Q8tPqQOSj4xL4Y1aTJoQcvuOz258yHq450toFaGmZBHkixxNrOrVT'
+secret_key = '4sorjQMmBN19KAgEDhNdQUZJaErCy3bPHWrhwp0NM155QNduZAthpCOKIh0sT7mA'
 
 
 client_name = 'ML'
 
 ticker_swap = 'ETHUSD_PERP'
-ticker_futures = 'ETHUSD_220325'
+ticker_futures = 'ETHUSD_211231'
 
 swap_side = 'sell'
 futures_side = 'buy'
 reduce_only = False
-total_amount = 1000
+total_amount = 6000
 section = 'COIN-M'
