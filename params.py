c516422c46752f1fa327d420ca91f226114cd7ee
Alexandr Boyko <BoykoA.A@yandex.ru>
2021-11-01 13:19:32 +0300

    up

--- params.py
+++ params.py
@@ -2,19 +2,18 @@
 
 
 
+#kkk
+api_key = 'CuUqMsENuKX4RMXNOeAUSChU9nYD3nNWnEYYd7x2UFFznRTD77wCmXhj80dD5TjE'
+secret_key = 'bPszoss42LPnlCEaxxRLjMrJNJ1bDQdWnpc0Q3fGRAt8Q6JIaEOgLdHMP3RumtxX'
 
-#SK
-api_key = 'qNlf8WQoTg3oYH9HDpEiQo3lJh0pRwBpodXHac0PPiOov93o3GknFIcYYW3ZurfN'
-secret_key = 'vVPMMbtKHMRdD4IF5CeIi182z54AthnfuSrjQSLHiO1WpckB0TvGVg1lQQPS0vTP'
 
+client_name = 'kkk'
 
-client_name = 'SK'
-
-ticker_swap = 'ETHUSD_PERP'
-ticker_futures = 'ETHUSD_220325'
+ticker_swap = 'BTCUSD_PERP'
+ticker_futures = 'BTCUSD_220325'
 
 swap_side = 'sell'
 futures_side = 'buy'
 reduce_only = False
-total_amount = 3000
+total_amount = 196
 section = 'COIN-M'
