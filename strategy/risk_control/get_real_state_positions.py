5f87bf9b4b480d9590b542d20b8992293a561173
andreymaltsev <a.maltsev@hashcib.com>
2021-11-08 21:16:32 +0300

    --

--- strategy/risk_control/get_real_state_positions.py
+++ strategy/risk_control/get_real_state_positions.py
@@ -9,13 +9,12 @@ class RealStatePositions:
         self.hyperparams_strategy = ProviderHyperParamsStrategy()
 
     def get_positions(self):
-        tickers_pos_usdt_m = {}
-        tickers_pos_coin_m = {}
+        tickers_pos = {}
         tickers_usdt_m = self.hyperparams_strategy.get_list_tickers(section='USDT-M', kind='quart')
         tickers_coin_m = self.hyperparams_strategy.get_list_tickers(section='COIN-M', kind='current') + \
                          self.hyperparams_strategy.get_list_tickers(section='COIN-M', kind='next')
         for ticker_usd in tickers_usdt_m:
-            tickers_pos_usdt_m[ticker_usd] = self.data_provider_usdt_m.get_amount_positions(ticker_usd)
+            tickers_pos[ticker_usd] = self.data_provider_usdt_m.get_amount_positions(ticker_usd)
         for ticker_coin in tickers_coin_m:
-            tickers_pos_coin_m[ticker_coin] = self.data_provider_coin_m.get_amount_positions(ticker_coin)
-        return {'USDT-M': tickers_pos_usdt_m, 'COIN-M': tickers_pos_coin_m}
+            tickers_pos[ticker_coin] = self.data_provider_coin_m.get_amount_positions(ticker_coin)
+        return tickers_pos
