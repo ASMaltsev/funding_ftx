878561b11a863fbff7513516328c5bd92cc4f62f
Alexandr Boyko <BoykoA.A@yandex.ru>
2021-11-08 18:41:17 +0300

    up

--- strategy/risk_control/get_real_state_positions.py
+++ strategy/risk_control/get_real_state_positions.py
@@ -1,12 +1,27 @@
 from strategy.hyperparams import ProviderHyperParamsStrategy
+from strategy.data_provider.binanace_provider.binance_data_provider import BinanceDataProvider
 
 
 class RealStatePositions:
 
     def __init__(self, provider_usdt_m, provider_coin_m):
-        self.provider_usdt_m = provider_usdt_m
-        self.provider_coin_m = provider_coin_m
+        self.data_provider = BinanceDataProvider('', '', 'USDT-M')
+        # self.provider_coin_m = BinanceDataProvider('', '', 'COIN-M')
         self.hyperparams_strategy = ProviderHyperParamsStrategy()
 
     def get_positions(self):
-        self.hyperparams_strategy.get_all_tickers(section='USDT-M')
+        tickers_usdtm = self.hyperparams_strategy.get_all_tickers(section='USDT-M')
+        tickers_coinm = self.hyperparams_strategy.get_all_tickers(section='COIN-M')
+
+        a = self.data_provider.get_amount_positions('ETHUSDT')
+        b = self.data_provider.get_amount_positions('ETHUSD_PERP')
+
+        print(a, b)
+
+
+
+
+
+
+
+        return tickers_usdtm
