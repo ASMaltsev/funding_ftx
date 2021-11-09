e271b42e2882ff7fdd1fde8d86a364b55853af86
andreymaltsev <a.maltsev@hashcib.com>
2021-11-09 17:46:43 +0300

    final logic

--- main.py
+++ main.py
@@ -1,28 +1,11 @@
-from strategy.alpha import FundingAlpha
 from strategy.executor import DadExecutor
-from strategy.data_provider.binanace_provider.binance_data_provider import BinanceDataProvider
-from strategy.risk_control.rebalancer import Rebalancer
+from strategy.logging import Logger
 
 api_key = ''
 secret_key = ''
+logger = Logger('Main').create()
 
-data_provider_usdt_m = BinanceDataProvider(api_key=api_key, secret_key=secret_key, section='USDT-M')
-data_provider_coin_m = BinanceDataProvider(api_key=api_key, secret_key=secret_key, section='COIN-M')
-
-instructions = FundingAlpha().decide()
-DadExecutor(api_key=api_key, secret_key=secret_key).execute(instructions)
-
-"""
-ticker_swap = 'ETHUSDT'
-ticker_futures = 'ETHUSDT_211231'
-
-swap_side = 'sell'
-futures_side = 'buy'
-reduce_only = False
-total_amount = 0.01
-section = 'USDT-M'
-
-BinanceExecutor(api_key=api_key, secret_key=secret_key, section=section, market_ticker=ticker_futures,
-                limit_ticker=ticker_swap, limit_side=swap_side, market_side=futures_side, total_amount=total_amount,
-                reduce_only=reduce_only).execute()
-"""
+try:
+    DadExecutor(api_key=api_key, secret_key=secret_key).execute()
+except Exception as e:
+    logger.error(e)
