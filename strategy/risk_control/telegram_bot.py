4a0143e7226575aecd6923f0f0bcb348373b1718
Alexandr Boyko <BoykoA.A@yandex.ru>
2021-11-09 20:11:20 +0300

    up

--- strategy/risk_control/telegram_bot.py
+++ strategy/risk_control/telegram_bot.py
@@ -1,9 +1,32 @@
+import telebot
+import ast
+import time
+from telebot import types
+
+
+token = '2116226519:AAEPqDgHtE6Rs-fTVttpeM7rwH3IiIWnb-Y'
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['ok'])
+def action():
+    self.bot.stop_bot()
+
 class TelegramBot:
 
+    """
+    @return: True if OK False otherwise
+    """
+
     def __init__(self):
-        self.token = ''
+        self.bot = bot
+        self.chat_id = -766294450
+
 
     def start(self, final_instructions, real_positions) -> bool:
-        """
-        @return: True if OK False otherwise
-        """
+        self.bot.send_message(chat_id=self.chat_id, text=real_positions)
+        self.bot.send_message(chat_id=self.chat_id, text=final_instructions)
+
+        try:
+            self.bot.polling(none_stop=False, interval=0, timeout=20)
+        except:
+            return True
