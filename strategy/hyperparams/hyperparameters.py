8077c7760ec2ff87358a2ca7247832e6f542b762
Alexandr Boyko <BoykoA.A@yandex.ru>
2021-11-10 17:27:33 +0300

    up

--- strategy/hyperparams/hyperparameters.py
+++ strategy/hyperparams/hyperparameters.py
@@ -2,7 +2,7 @@ strategy_hyperparams = {
     'base_fr_earn': 0.1095,
     'A': 0.04,
     'k': 0.0495,
-    'time_exit': 9,
+    'time_exit': 109,
     'save_time': lambda x: x + 5,
     'USDT-M':
         {
