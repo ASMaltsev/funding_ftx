aaa16c396d41f46572dc3ace2867f9bcc963b1df
andreymaltsev <a.maltsev@hashcib.com>
2021-11-15 16:20:29 +0300

    fixes

--- strategy/executor/binance_executor/dad_executor.py
+++ strategy/executor/binance_executor/dad_executor.py
@@ -36,8 +36,9 @@ class DadExecutor:
                 batches = self._generate_batches(executor_instructions)
                 logger.info(msg='Batches: ', extra=dict(batches=batches))
                 for batch in batches:
+                    provider = self.data_provider_usdt_m if batch['section'] == 'USDT-M' else self.data_provider_coin_m
                     del batch['section']
-                    BinanceExecutor(self.data_provider_coin_m, **batch).execute()
+                    BinanceExecutor(provider, **batch).execute()
 
     def _generate_instructions(self):
         instructions = FundingAlpha().decide()
