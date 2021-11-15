19befe0f627f8656d02163aa2bb522ff1cf5cce2
andreymaltsev <a.maltsev@hashcib.com>
2021-11-15 12:22:29 +0300

    del logs

--- main.py
+++ main.py
@@ -6,7 +6,5 @@ api_key = 'IdPorsZNdskqCUNbO5aN0w6TY67Kfl0syZjHDV3ZP9tOMuM6k3KzovNizMKmBpix'
 secret_key = '7qE1lC0fVpNF7i9Lb08odC1HaV6m2LILmzy2SSEnAXTwqOVaJhqA8cVz1tzPzP0A'
 
 logger = Logger('Main').create()
-try:
-    DadExecutor(api_key=api_key, secret_key=secret_key).execute()
-except Exception as e:
-    logger.error(e)
+DadExecutor(api_key=api_key, secret_key=secret_key).execute()
+
