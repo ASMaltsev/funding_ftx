521696d563a067be6d9408b79b5862be470ff5d5
Alexandr Boyko <BoykoA.A@yandex.ru>
2021-11-15 12:21:14 +0300

    up

--- strategy/hyperparams/hyperparameters.py
+++ strategy/hyperparams/hyperparameters.py
@@ -27,32 +27,32 @@ strategy_hyperparams = {
                         },
                 }
         },
-    'COIN-M':
-        {
-            'share':
-                {
-                    'current': 0.475,
-                    'next': 0.475
-                },
-            'assets':
-                {
-                    # 'ETH':
-                    #     {
-                    #         'perp': 'ETHUSD_PERP',
-                    #         'current': 'ETHUSD_211231',
-                    #         'next': 'ETHUSD_220325',
-                    #         'min_batch_size': 10,
-                    #     },
-                    # 'BTC':
-                    #
-                    #     {
-                    #         'perp': 'BTCUSD_PERP',
-                    #         'current': 'BTCUSD_211231',
-                    #         'next': 'BTCUSD_220325',
-                    #         'min_batch_size': 5,
-                    #     }
-                }
-        }
+    # 'COIN-M':
+    #     {
+    #         'share':
+    #             {
+    #                 'current': 0.475,
+    #                 'next': 0.475
+    #             },
+    #         'assets':
+    #             {
+    #                 'ETH':
+    #                     {
+    #                         'perp': 'ETHUSD_PERP',
+    #                         'current': 'ETHUSD_211231',
+    #                         'next': 'ETHUSD_220325',
+    #                         'min_batch_size': 10,
+    #                     },
+    #                 'BTC':
+    #
+    #                     {
+    #                         'perp': 'BTCUSD_PERP',
+    #                         'current': 'BTCUSD_211231',
+    #                         'next': 'BTCUSD_220325',
+    #                         'min_batch_size': 5,
+    #                     }
+    #             }
+    #     }
 }
 
 account_hyperparams = {
