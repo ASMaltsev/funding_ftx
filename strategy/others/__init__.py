b274aa9568c6f313c999e15788381bc9c22f35e7
andreymaltsev <a.maltsev@hashcib.com>
2021-11-16 12:36:57 +0300

    add logging to grafana

--- strategy/others/__init__.py
+++ strategy/others/__init__.py
@@ -1,4 +1,4 @@
 from strategy.others.inverse_operations import inverse_operation
-from strategy.others.constants import API_KEY, SECRET_KEY, STATHAM_TOKEN, STATHAM_CHAT_ID
+from strategy.others.constants import API_KEY, SECRET_KEY, STATHAM_TOKEN, STATHAM_CHAT_ID, CLIENT_NAME
 
-__all__ = ['inverse_operation', 'API_KEY', 'SECRET_KEY', 'STATHAM_TOKEN', 'STATHAM_CHAT_ID']
+__all__ = ['inverse_operation', 'API_KEY', 'SECRET_KEY', 'STATHAM_TOKEN', 'STATHAM_CHAT_ID', 'CLIENT_NAME']
