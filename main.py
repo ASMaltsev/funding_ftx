4af65565b7794b6a407f4166533fb323067d5b21
andreymaltsev <a.maltsev@hashcib.com>
2021-12-16 16:46:30 +0300

    bug fix

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
 from strategy.executor import DadExecutor
-from strategy.others import API_KEY, SECRET_KEY, LABEL
+from strategy.others import LABEL
 from strategy.others.constants import get_secret
 
 get_secret(LABEL)
 
-print(API_KEY)
-print(SECRET_KEY)
-DadExecutor(api_key=API_KEY, secret_key=SECRET_KEY).execute()
+DadExecutor().execute()
