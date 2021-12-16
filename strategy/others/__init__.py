014f6bd9da27625f3de85c69c23230c36312e47a
andreymaltsev <a.maltsev@hashcib.com>
2021-12-16 16:58:42 +0300

    bug fix

--- strategy/others/__init__.py
+++ strategy/others/__init__.py
@@ -1,6 +1,6 @@
 from strategy.others.inverse_operations import inverse_operation
-from strategy.others.constants import API_KEY, SECRET_KEY, STATHAM_TOKEN, STATHAM_CHAT_ID, LABEL, HOSTS, SECTION, \
+from strategy.others.constants import STATHAM_TOKEN, STATHAM_CHAT_ID, LABEL, HOSTS, SECTION, \
     USER_NAME, USER_PASS, HOST_DB, ELASTIC_NAME, ELASTIC_PASS
 
-__all__ = ['inverse_operation', 'API_KEY', 'SECRET_KEY', 'STATHAM_TOKEN', 'STATHAM_CHAT_ID', 'LABEL',
+__all__ = ['inverse_operation', 'STATHAM_TOKEN', 'STATHAM_CHAT_ID', 'LABEL',
            'HOSTS', 'SECTION', 'USER_NAME', 'USER_PASS', 'HOST_DB', 'ELASTIC_NAME', 'ELASTIC_PASS']
